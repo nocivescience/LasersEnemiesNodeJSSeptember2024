5d8d46512dba6be63658b7bfa427dfa2dc547bcf
ricardo <nocive.science@gmail.com>
2024-09-22 08:10:30 -0300

    primer commit al master

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors'); // Importa el paquete CORS
+const app = express();
+
+// Configuración de EJS como motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+// Middleware CORS para permitir solicitudes de otros orígenes
+app.use(cors());
+
+// Rutas
+app.get('/', (req, res) => {
+  // Renderizar la plantilla 'index.ejs'
+  res.render('index', { title: 'Página de Inicio' });
+});
+
+// Una ruta que podría ser llamada desde otro origen
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Este es el mensaje desde la API' });
+});
+
+// Inicia el servidor
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
